32901c4030325ba31e4569dfeb2b28e618accb66
nguyenkhoa <nguyenkhoa72.work@gmail.com>
2023-02-09 20:04:46 +0700

    feat: close channel

--- client/client_cmd.go
+++ client/client_cmd.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"math"
 	"math/big"
 
 	channelTypes "github.com/AstraProtocol/channel/x/channel/types"
@@ -16,7 +15,7 @@ import (
 	"github.com/m25-lab/lightning-network-node/rpc/pb"
 )
 
-func CreateCommitmentFromA(amountA int64, amountB int64, secret string) (channelTypes.MsgCommitment, string, string) {
+func CreateCommitmentFromA(amountA int64, amountB int64, secret string, timelock uint64, seq int) (channelTypes.MsgCommitment, string, string) {
 	cfg := &Config{
 		ChainId:               "channel",
 		Endpoint:              "http://0.0.0.0:26657",
@@ -33,10 +32,10 @@ func CreateCommitmentFromA(amountA int64, amountB int64, secret string) (channel
 	multisigAddr, multiSigPubkey, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
 
 	partACommitment := channelTypes.MsgCommitment{
-		ChannelID:      c.RpcClient().ChainID,
+		ChannelID:      multisigAddr + ":token:" + fmt.Sprint(seq),
 		Creator:        multisigAddr,
 		From:           multisigAddr,
-		Timelock:       10,
+		Timelock:       uint64(timelock),
 		ToTimelockAddr: BAccount.AccAddress().String(),
 		CoinToCreator: &types.Coin{
 			Denom:  "token",
@@ -107,7 +106,7 @@ func CreateCommitmentFromB(partACommitment channelTypes.MsgCommitment, aAddress 
 
 	// @Description: Create commitment B
 	partBCommitment := channelTypes.MsgCommitment{
-		ChannelID:      c.RpcClient().ChainID,
+		ChannelID:      partACommitment.ChannelID,
 		Creator:        multisigAddr,
 		From:           multisigAddr,
 		Timelock:       partACommitment.Timelock,
@@ -165,7 +164,7 @@ func StoreCommitmentFromA(partBCommitment channelTypes.MsgCommitment, bAddress s
 	fmt.Println(response, err)
 }
 
-func OpenChannelFromA(amountA int64, amountB int64) string {
+func OpenChannelFromA(amountA int64, amountB int64, seq int) string {
 	cfg := &Config{
 		ChainId:               "channel",
 		Endpoint:              "http://0.0.0.0:26657",
@@ -186,25 +185,25 @@ func OpenChannelFromA(amountA int64, amountB int64) string {
 	fmt.Println("PublicKey", AAccount.PublicKey())
 
 	multisigAddr, multiSigPubkey, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
-	transfer(c, AAccount.PrivateKeyToString(), multisigAddr)
+	transfer(c, AAccount.PrivateKeyToString(), multisigAddr, big.NewInt(1))
 	fmt.Println("multisigAddr", multisigAddr)
 
 	openChannelRequest := channel.SignMsgRequest{
-		Msg: channelTypes.NewMsgOpenChannel(
-			multisigAddr,
-			AAccount.AccAddress().String(),
-			BAccount.AccAddress().String(),
-			&types.Coin{
+		Msg: &channelTypes.MsgOpenChannel{
+			Creator: multisigAddr,
+			PartA:   AAccount.AccAddress().String(),
+			PartB:   BAccount.AccAddress().String(),
+			CoinA: &types.Coin{
 				Denom:  "token",
 				Amount: types.NewInt(amountA),
 			},
-			&types.Coin{
+			CoinB: &types.Coin{
 				Denom:  "token",
 				Amount: types.NewInt(amountB),
 			},
-			multisigAddr,
-			"1",
-		),
+			MultisigAddr: multisigAddr,
+			Sequence:     fmt.Sprint(seq),
+		},
 		GasLimit: 200000,
 		GasPrice: "0token",
 	}
@@ -351,16 +350,251 @@ func OpenChannelFromB(channelId string) {
 	}
 	fmt.Println("tx openchannel result code", txResult.Code)
 	fmt.Println(txResult)
+}
+
+func FundFromA(amount int64, secret string, timelock uint64, seq int) (channelTypes.MsgFund, string, string) {
+	cfg := &Config{
+		ChainId:               "channel",
+		Endpoint:              "http://0.0.0.0:26657",
+		LightningNodeEndpoint: "0.0.0.0:2525",
+		CoinType:              60,
+		PrefixAddress:         "cosmos",
+		TokenSymbol:           "token",
+	}
+
+	c := NewClient(cfg)
+	acc := c.NewAccountClient()
+	AAccount, _ := acc.ImportAccount("excuse quiz oyster vendor often spray day vanish slice topic pudding crew promote floor shadow best subway slush slender good merit hollow certain repeat")
+	BAccount, _ := acc.ImportAccount("claim market flip canoe wreck maid recipe bright fuel slender ladder album behind repeat come trophy come vicious frown prefer height unknown thank damp")
+	multisigAddr, multiSigPubkey, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
+
+	partACommitment := channelTypes.MsgFund{
+		ChannelID: multisigAddr + ":cosmos:" + fmt.Sprint(seq),
+		Creator:   multisigAddr,
+		From:      AAccount.AccAddress().String(),
+		CoinToHtlc: &types.Coin{
+			Denom:  "token",
+			Amount: types.NewInt(amount),
+		},
+		Hashcode: secret,
+		Timelock: fmt.Sprint(timelock),
+		Multisig: multisigAddr,
+	}
+
+	commitmentA := channel.SignMsgRequest{
+		Msg:      &partACommitment,
+		GasLimit: 200000,
+		GasPrice: "0token",
+	}
+
+	channelClient := c.NewChannelClient()
+
+	strSig, err := channelClient.SignMultisigTxFromOneAccount(commitmentA, AAccount, multiSigPubkey)
+	if err != nil {
+		panic(err)
+	}
+
+	return partACommitment, AAccount.AccAddress().String(), strSig
+}
+
+func AcceptFundFromB(fundCommitment channelTypes.MsgFund, aAddress string, aSignature string, secret string) (channelTypes.MsgAcceptfund, string, string) {
+	cfg := &Config{
+		ChainId:               "channel",
+		Endpoint:              "http://0.0.0.0:26657",
+		LightningNodeEndpoint: "0.0.0.0:2525",
+		CoinType:              60,
+		PrefixAddress:         "cosmos",
+		TokenSymbol:           "token",
+	}
+
+	c := NewClient(cfg)
+	acc := c.NewAccountClient()
+	AAccount, _ := acc.ImportAccount("excuse quiz oyster vendor often spray day vanish slice topic pudding crew promote floor shadow best subway slush slender good merit hollow certain repeat")
+	BAccount, _ := acc.ImportAccount("claim market flip canoe wreck maid recipe bright fuel slender ladder album behind repeat come trophy come vicious frown prefer height unknown thank damp")
+	multisigAddr, multiSigPubkey, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
+
+	partBCommitment := channelTypes.MsgAcceptfund{
+		ChannelID:        fundCommitment.ChannelID,
+		Creator:          fundCommitment.Creator,
+		From:             fundCommitment.From,
+		CoinToAcceptSide: fundCommitment.CoinToHtlc,
+		Hashcode:         secret,
+		Timelock:         fundCommitment.Timelock,
+		Multisig:         multisigAddr,
+	}
+
+	commitmentB := channel.SignMsgRequest{
+		Msg:      &partBCommitment,
+		GasLimit: 200000,
+		GasPrice: "0token",
+	}
+
+	channelClient := c.NewChannelClient()
+
+	strSig, err := channelClient.SignMultisigTxFromOneAccount(commitmentB, AAccount, multiSigPubkey)
+	if err != nil {
+		panic(err)
+	}
+
+	return partBCommitment, BAccount.AccAddress().String(), strSig
+}
+
+func StoreAcceptFundCommitmentFromA(acceptFundCommitment channelTypes.MsgAcceptfund, bAddress string, bSignature string) {
+	cfg := &Config{
+		ChainId:               "channel",
+		Endpoint:              "http://0.0.0.0:26657",
+		LightningNodeEndpoint: "0.0.0.0:2525",
+		CoinType:              60,
+		PrefixAddress:         "cosmos",
+		TokenSymbol:           "token",
+	}
+
+	c := NewClient(cfg)
+	acc := c.NewAccountClient()
+	AAccount, _ := acc.ImportAccount("excuse quiz oyster vendor often spray day vanish slice topic pudding crew promote floor shadow best subway slush slender good merit hollow certain repeat")
+	BAccount, _ := acc.ImportAccount("claim market flip canoe wreck maid recipe bright fuel slender ladder album behind repeat come trophy come vicious frown prefer height unknown thank damp")
+	multisigAddr, _, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
+
+	fmt.Println(acceptFundCommitment, bAddress, bSignature)
+	transfer(c, AAccount.PrivateKeyToString(), multisigAddr, big.NewInt(1))
+}
+
+func CloseChannel(amountA int64, amoutB int64, seq int) (channelTypes.MsgCloseChannel, string, string) {
+	cfg := &Config{
+		ChainId:               "channel",
+		Endpoint:              "http://0.0.0.0:26657",
+		LightningNodeEndpoint: "0.0.0.0:2525",
+		CoinType:              60,
+		PrefixAddress:         "cosmos",
+		TokenSymbol:           "token",
+	}
+
+	c := NewClient(cfg)
+	acc := c.NewAccountClient()
+	AAccount, _ := acc.ImportAccount("excuse quiz oyster vendor often spray day vanish slice topic pudding crew promote floor shadow best subway slush slender good merit hollow certain repeat")
+	BAccount, _ := acc.ImportAccount("claim market flip canoe wreck maid recipe bright fuel slender ladder album behind repeat come trophy come vicious frown prefer height unknown thank damp")
+	multisigAddr, multiSigPubkey, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
+
+	closeChannelMsg := channelTypes.MsgCloseChannel{
+		ChannelID: multisigAddr + ":token:" + fmt.Sprint(seq),
+		Creator:   multisigAddr,
+		ToA:       AAccount.AccAddress().String(),
+		ToB:       BAccount.AccAddress().String(),
+		CoinA: &types.Coin{
+			Denom:  "token",
+			Amount: types.NewInt(amountA),
+		},
+		CoinB: &types.Coin{
+			Denom:  "token",
+			Amount: types.NewInt(amoutB),
+		},
+	}
+	closeChannelRequest := channel.SignMsgRequest{
+		Msg:      &closeChannelMsg,
+		GasLimit: 200000,
+		GasPrice: "0token",
+	}
+
+	channelClient := c.NewChannelClient()
+
+	strSig, err := channelClient.SignMultisigTxFromOneAccount(closeChannelRequest, AAccount, multiSigPubkey)
+	if err != nil {
+		panic(err)
+	}
+
+	return closeChannelMsg, BAccount.AccAddress().String(), strSig
+}
+
+func AcceptCloseChannel(closeChannel channelTypes.MsgCloseChannel, aAddress string, aSignature string) {
+	cfg := &Config{
+		ChainId:               "channel",
+		Endpoint:              "http://0.0.0.0:26657",
+		LightningNodeEndpoint: "0.0.0.0:2525",
+		CoinType:              60,
+		PrefixAddress:         "cosmos",
+		TokenSymbol:           "token",
+	}
+
+	c := NewClient(cfg)
+	acc := c.NewAccountClient()
+	AAccount, _ := acc.ImportAccount("excuse quiz oyster vendor often spray day vanish slice topic pudding crew promote floor shadow best subway slush slender good merit hollow certain repeat")
+	BAccount, _ := acc.ImportAccount("claim market flip canoe wreck maid recipe bright fuel slender ladder album behind repeat come trophy come vicious frown prefer height unknown thank damp")
+	_, multiSigPubkey, _ := acc.CreateMulSigAccountFromTwoAccount(AAccount.PublicKey(), BAccount.PublicKey(), 2)
+
+	acceptCloseChannelMsg := channelTypes.MsgCloseChannel{
+		ChannelID: closeChannel.ChannelID,
+		Creator:   closeChannel.Creator,
+		ToA:       closeChannel.ToA,
+		ToB:       closeChannel.ToB,
+		CoinA:     closeChannel.CoinA,
+		CoinB:     closeChannel.CoinB,
+	}
+
+	acceptCloseChannelRequest := channel.SignMsgRequest{
+		Msg:      &acceptCloseChannelMsg,
+		GasLimit: 200000,
+		GasPrice: "0token",
+	}
+
+	channelClient := c.NewChannelClient()
+
+	signList := make([][]signingTypes.SignatureV2, 0)
+
+	signByte1, err := common.SignatureJsonDecoder(c.RpcClient().TxConfig, aSignature)
+	if err != nil {
+		panic(err)
+	}
+
+	signList = append(signList, signByte1)
+
+	strSig2, err := channelClient.SignMultisigTxFromOneAccount(acceptCloseChannelRequest, BAccount, multiSigPubkey)
+	if err != nil {
+		panic(err)
+	}
+	signByte2, err := common.SignatureJsonDecoder(c.RpcClient().TxConfig, strSig2)
+	if err != nil {
+		panic(err)
+	}
+	signList = append(signList, signByte2)
+
+	newTx := common.NewMultisigTxBuilder(c.RpcClient(), nil, 200000, "0token", 0, 2)
+	txBuilderMultiSign, err := newTx.BuildUnsignedTx(acceptCloseChannelRequest.Msg)
+	if err != nil {
+		panic(err)
+	}
+
+	err = newTx.GenerateMultisig(txBuilderMultiSign, multiSigPubkey, signList)
+	if err != nil {
+		panic(err)
+	}
+
+	txJson, err := common.TxBuilderJsonEncoder(c.RpcClient().TxConfig, txBuilderMultiSign)
+	if err != nil {
+		panic(err)
+	}
+
+	txByte, err := common.TxBuilderJsonDecoder(c.RpcClient().TxConfig, txJson)
+	if err != nil {
+		panic(err)
+	}
+
+	txHash := common.TxHash(txByte)
+	fmt.Println(txHash)
+
+	txResult, err := c.RpcClient().BroadcastTxSync(txByte)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(txResult)
 
 }
 
-func transfer(c *Client, privateKey string, toAddress string) {
+func transfer(c *Client, privateKey string, toAddress string, value *big.Int) {
 	bankClient := c.NewBankClient()
-	amount := big.NewInt(0).Mul(big.NewInt(10), big.NewInt(0).SetUint64(uint64(math.Pow10(1))))
 	request := &bank.TransferRequest{
 		PrivateKey: privateKey,
 		Receiver:   toAddress,
-		Amount:     amount,
+		Amount:     value,
 		GasLimit:   200000,
 		GasPrice:   "0token",
 	}
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -5,18 +5,26 @@ import (
 )
 
 func main() {
-	partACommitment, addressA, signature := client.CreateCommitmentFromA(50, 50, "secret from A")
+	partACommitment, addressA, signature := client.CreateCommitmentFromA(50, 50, "secret from A", 10, 1)
 	partBCommitment, addressB, signature := client.CreateCommitmentFromB(partACommitment, addressA, signature, "secret from B")
 	client.StoreCommitmentFromA(partBCommitment, addressB, signature)
 
-	channelId := client.OpenChannelFromA(50, 50)
+	channelId := client.OpenChannelFromA(50, 50, 1)
 	client.OpenChannelFromB(channelId)
 
-	partACommitment, addressA, signature = client.CreateCommitmentFromA(40, 60, "secret from AA")
+	partACommitment, addressA, signature = client.CreateCommitmentFromA(40, 60, "secret from AA", 10, 1)
 	partBCommitment, addressB, signature = client.CreateCommitmentFromB(partACommitment, addressA, signature, "secret from BB")
 	client.StoreCommitmentFromA(partBCommitment, addressB, signature)
 
-	partACommitment, addressA, signature = client.CreateCommitmentFromA(70, 30, "secret from AA")
+	partACommitment, addressA, signature = client.CreateCommitmentFromA(70, 30, "secret from AA", 10, 1)
 	partBCommitment, addressB, signature = client.CreateCommitmentFromB(partACommitment, addressA, signature, "secret from BB")
 	client.StoreCommitmentFromA(partBCommitment, addressB, signature)
+
+	partAFundCommitment, addressA, signature := client.FundFromA(10, "secret from AA", 10, 1)
+	partBAcceptFundCommitment, addressB, signature := client.AcceptFundFromB(partAFundCommitment, addressA, signature, "secret from BB")
+	client.StoreAcceptFundCommitmentFromA(partBAcceptFundCommitment, addressB, signature)
+
+	closeChannel, addressA, signature := client.CloseChannel(10, 10, 1)
+	client.AcceptCloseChannel(closeChannel, addressA, signature)
+
 }
